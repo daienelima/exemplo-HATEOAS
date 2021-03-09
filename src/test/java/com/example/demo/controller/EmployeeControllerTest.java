11f6198b3a69c786155fcac29a8e29ea99ab8fb1
Daiene Lima <daiene.m17@gmail.com>
2021-03-09 20:57:23 -0300

    testes

--- src/test/java/com/example/demo/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/demo/controller/EmployeeControllerTest.java
@@ -11,10 +11,12 @@ import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +26,14 @@ import com.example.demo.model.Employee;
 import com.example.demo.model.EmployeeTest;
 import com.example.demo.repository.EmployeeRepository;
 
+//@RunWith(MockitoJUnitRunner.class)
 public class EmployeeControllerTest {
 
 	@Mock
-	EmployeeRepository employeeRepository;
+	private EmployeeRepository employeeRepository;
 	
 	@InjectMocks
-	EmployeeController employeeController;
+	private EmployeeController employeeController;
 
 	@Before
 	public void setup() {
@@ -59,7 +62,7 @@ public class EmployeeControllerTest {
 	@Test
 	public void newEmployee_test() {
 		Employee employee = EmployeeTest.create();
-		when(employeeRepository.save(employee)).thenReturn(employee);
+		when(employeeRepository.save(Mockito.any(Employee.class))).thenReturn(employee);
 		ResponseEntity<Employee> newEmployee = employeeController.newEmployee(employee);
 		assertEquals(employee.getName(), newEmployee.getBody().getName());
 		assertEquals(employee.getRole(), newEmployee.getBody().getRole());
@@ -85,6 +88,23 @@ public class EmployeeControllerTest {
 	public void deleteEmployee() {
 		employeeController.deleteEmployee(1L);
 		Mockito.verify(employeeRepository, Mockito.times(1)).deleteById(1L);
+	}
+	
+	@Test
+	public void spyTest() {
+		List<Employee> employees = Mockito.spy(new ArrayList<Employee>());
+		Employee one = new Employee("Bilbo Baggins", "burglar");
+		Employee two = new Employee("Frodo Baggins", "thief");
+		
+		employees.add(one);
+		employees.add(two);
+		
+		Mockito.verify(employees).add(one);
+	    Mockito.verify(employees).add(two);
+	    assertEquals(2, employees.size());
+
+	    Mockito.doReturn(100).when(employees).size();
+	    assertEquals(100, employees.size());
 	}
 	
 	private List<Employee> createEmployeeMock() {
